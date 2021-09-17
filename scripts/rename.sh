43baa7e32ad0dc63fe6f6fc09e5ca759af7b545c
David Griffin <findgriffin@gmail.com>
2021-09-17 13:57:40 -0700

    rename.sh: Fixes to ensure rename works properly.

--- scripts/rename.sh
+++ scripts/rename.sh
@@ -7,8 +7,13 @@ fi
 
 echo "Renaming stub to $1"
 
-sed -i '' -e "s/stub/$1/g" run.py 
-sed -i '' -e "s/stub/$1/g" tests/test_stub.py
-
 git mv stub $1
+git mv $1/stub.py $1/$1.py 
 git mv tests/test_stub.py tests/test_$1.py
+
+sed -i '' -e "s/stub/$1/g" run.py 
+sed -i '' -e "s/stub/$1/g" tests/test_$1.py
+git add run.py tests/test_$1.py
+
+make
+git commit -m "Renamed stub -> $1"
