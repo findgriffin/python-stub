f618dd384ada37b6d39098f3a33de953a89dd19b
David Griffin <findgriffin@gmail.com>
2021-09-22 14:50:06 -0700

    Fix Makefile: specify source directory for coverage.
      * This stops random stuff, like imports and run.py
        from being included in coverage statistics.

--- scripts/rename.sh
+++ scripts/rename.sh
@@ -13,7 +13,8 @@ git mv tests/test_stub.py tests/test_$1.py
 
 sed -i '' -e "s/stub/$1/g" run.py 
 sed -i '' -e "s/stub/$1/g" tests/test_$1.py
-git add run.py tests/test_$1.py
+sed -i '' -e "s/stub/$1/g" Makefile
+git add run.py tests/test_$1.py Makefile
 
 make
 git commit -m "Renamed stub -> $1"
